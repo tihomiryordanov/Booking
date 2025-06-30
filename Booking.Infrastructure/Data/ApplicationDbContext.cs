9a1542b3668207b7ba3e6e3ff45f17ad7c6b442b
Tihomir Yordanov <tyordanov2@sensata.com>
2025-06-30 21:59:56 +0300

    Add Villa controller. Add Villa Index View. Change bootstrap to newest version.

--- Booking.Infrastructure/Data/ApplicationDbContext.cs
+++ Booking.Infrastructure/Data/ApplicationDbContext.cs
@@ -14,8 +14,40 @@ namespace Booking.Infrastructure.Data
         public DbSet<Villa> Villas { get; set; } = null!;
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
-            
+            //base.OnModelCreating(modelBuilder);
+            //Seeding the database with initial data
+            modelBuilder.Entity<Villa>().HasData(
+                      new Villa
+                      {
+                          Id = 1,
+                          Name = "Royal Villa",
+                          Description = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                          ImageUrl = "https://placehold.co/600x400",
+                          Occupancy = 4,
+                          Price = 200,
+                          SquareFeet = 550,
+                      },
+                    new Villa
+                    {
+                        Id = 2,
+                        Name = "Premium Pool Villa",
+                        Description = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                        ImageUrl = "https://placehold.co/600x401",
+                        Occupancy = 4,
+                        Price = 300,
+                        SquareFeet = 550,
+                    },
+                    new Villa
+                    {
+                        Id = 3,
+                        Name = "Luxury Pool Villa",
+                        Description = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                        ImageUrl = "https://placehold.co/600x402",
+                        Occupancy = 4,
+                        Price = 400,
+                        SquareFeet = 750,
+                    }
+            );
         }
     }
     
---
+++ Booking.Web/Controllers/VillaController.cs
@@ -0,0 +1,20 @@
+﻿using Booking.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Booking.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        // add db context here if needed
+        private readonly ApplicationDbContext _context;
+        public VillaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var villas = _context.Villas.ToList();
+            return View(villas);
+        }
+    }
+}
